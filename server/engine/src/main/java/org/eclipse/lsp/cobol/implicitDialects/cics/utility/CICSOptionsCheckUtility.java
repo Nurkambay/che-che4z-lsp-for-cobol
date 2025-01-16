03c6de03baa491e373b57549c3bae702fe71cb97
mm-broadcom <michael.messer@broadcom.com>
2025-01-16 10:48:28 -0500

    feat: CICS SIGNON improvements (#2607)

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
@@ -118,6 +118,9 @@ public class CICSOptionsCheckUtility {
         CICSReadNextReadPrevOptionsUtility.RULE_INDEX,
         new CICSReadNextReadPrevOptionsUtility(context, errors));
     optionsMap.put(
+        CICSSignonOptionsCheckUtility.RULE_INDEX,
+        new CICSSignonOptionsCheckUtility(context, errors));
+    optionsMap.put(
         CICSForceOptionsCheckUtility.RULE_INDEX,
         new CICSForceOptionsCheckUtility(context, errors));
     optionsMap.put(
