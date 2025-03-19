ac6711dff1ccfb1edf6748c1aaf780f321638764
Leonid Baranov <leonid.baranov@broadcom.com>
2025-03-19 17:18:43 +0100

    Fix tests

--- clients/cobol-lsp-vscode-extension/src/test/suite/lsp.spec.test.ts
+++ clients/cobol-lsp-vscode-extension/src/test/suite/lsp.spec.test.ts
@@ -539,21 +539,17 @@ suite("Integration Test Suite", function () {
     const copybookUri = await helper.getUri(copybookPath);
 
     await helper.waitFor(
-      () => vscode.languages.getDiagnostics(progUri).length === 2,
+      () => vscode.languages.getDiagnostics(progUri).length === 1,
     );
     const diagnostics = vscode.languages.getDiagnostics(progUri);
     assert.strictEqual(
       diagnostics.length,
-      2,
+      1,
       "got: " + JSON.stringify(diagnostics),
     );
     helper.hasDiagnosticMatches(
       progUri,
       (d) => d.message === "Errors inside the copybook",
-    );
-    helper.hasDiagnosticMatches(
-      progUri,
-      (d) => d.message === "Implicit EXIT PROGRAM statement executed",
     );
 
     // No diagnostic for copybook so far
