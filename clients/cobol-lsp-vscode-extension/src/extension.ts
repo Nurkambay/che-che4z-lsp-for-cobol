343021559c9e507041012698c3beda900bba51fe
slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
2026-03-24 17:36:41 +0100

    feat: Defer control flow analysis if unreachable code detection is off (#2983)

--- clients/cobol-lsp-vscode-extension/src/extension.ts
+++ clients/cobol-lsp-vscode-extension/src/extension.ts
@@ -141,6 +141,7 @@ export async function activate(
       log: true,
     }),
   );
+  context.subscriptions.push(analysisService);
   languageClientService.addNotificationHandler(
     "cfast/ready",
     analysisService.makeControlFlowAstNotificationHandler(),
