2215965707f36b19cba3ac618864b18ff76b20fa
Leonid Baranov <leonid.baranov@broadcom.com>
2026-09-10 16:02:52 +0200

    Initial

---
+++ clients/idms-dialect-support/src/engine/parsing.ts
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2026 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom - initial API and implementation
+ */
+import * as vscode from "vscode";
+import {
+  BaseErrorListener,
+  RecognitionException,
+  Token,
+  Recognizer,
+  ATNSimulator,
+} from "antlr4ng";
+import { ParseError } from "./model";
+
+export class CollectingErrorListener extends BaseErrorListener {
+  public readonly errors: ParseError[] = [];
+
+  syntaxError<S extends Token, T extends ATNSimulator>(
+    _recognizer: Recognizer<T>,
+    offendingSymbol: S | null,
+    line: number,
+    charPositionInLine: number,
+    msg: string,
+    _e: RecognitionException | null,
+  ): void {
+    this.errors.push({
+      line,
+      column: charPositionInLine,
+      message: msg,
+      range: this.getRangeForSyntaxError(
+        offendingSymbol,
+        line,
+        charPositionInLine,
+      ),
+    });
+  }
+
+  private getRangeForSyntaxError(
+    offendingSymbol: Token | null,
+    line: number,
+    charPositionInLine: number,
+  ) {
+    const tokenLength = offendingSymbol
+      ? offendingSymbol.stop - offendingSymbol.start + 1
+      : 0;
+    return new vscode.Range(
+      line - 1,
+      charPositionInLine,
+      line - 1,
+      charPositionInLine + tokenLength,
+    );
+  }
+}
+
+function concatResults<T>(r1: T[] | null, r2: T[] | null): T[] {
+  return [...(r1 ?? []), ...(r2 ?? [])];
+}
